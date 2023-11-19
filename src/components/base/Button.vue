0d291aa2196e935963c088e901a5c201cdc317d8
Bagus TRIANURDIN <btstargroup@gmail.com>
2023-11-19 14:50:49 +0700

    fiix(postcss): change postcss extention to mjs

--- src/components/base/Button.vue
+++ src/components/base/Button.vue
@@ -1,6 +1,6 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <button class="bg-cyan-500 text-cyan-700 h-8 px-2 rounded-md">
+  <button class="bg-cyan-500 text-cyan-700 h-10 px-3 rounded-md">
     <slot />
   </button>
 </template>
